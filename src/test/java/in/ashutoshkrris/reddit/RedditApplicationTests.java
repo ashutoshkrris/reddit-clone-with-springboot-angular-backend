a5bdb9fd426b1391ffea4acc3f19749b72ecb177
Ashutosh Krishna <ashutosh.krishna@thoughtworks.com>
2024-09-14 23:09:42 +0530

    CM-1 | Ashutosh | Initialise Project

---
+++ src/test/java/in/ashutoshkrris/reddit/RedditApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ashutoshkrris.reddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
